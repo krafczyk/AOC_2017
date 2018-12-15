312d75a457c577e5fce87c1083b1081d5856397e
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-14 21:45:46 -0600

    Initial task 2

---
+++ scripts/run_task_2_tests.sh
@@ -0,0 +1,5 @@
+./bin/task_2 -i tests/task_2_1.txt -t 6
+./bin/task_2 -i tests/task_2_2.txt -t 0
+./bin/task_2 -i tests/task_2_3.txt -t 4
+./bin/task_2 -i tests/task_2_4.txt -t 12
+./bin/task_2 -i tests/task_2_5.txt -t 4
