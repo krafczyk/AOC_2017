2760f89e4bec0e999dfaaf2705ac56cd2a37dd23
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-14 18:35:35 -0600

    Add utilities

---
+++ utilities/utilities.h
@@ -0,0 +1,40 @@
+#include <algorithm>
+
+//template<typename T>
+//concept Iterable 
+
+#if defined(__GNUC__) && not defined(YCM)
+// Shweeeeeeeeet
+
+template<typename T>
+concept bool ConstIterable = requires(T cont) {
+	{ cont.begin() } -> typename T::const_iterator;
+	{ cont.end() } -> typename T::const_iterator;
+};
+
+template<ConstIterable container, typename type>
+bool hasElement(const container& cont, const type& el) {
+	if(std::find(cont.begin(), cont.end(), el) == cont.end()) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+#else
+// BOOORING
+template<typename iter, typename type>
+bool hasElementHelper(const iter& begin, const iter& end, const type& el) {
+	if(std::find(begin, end, el) == end) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+template<typename container, typename type>
+bool hasElement(const container& cont, const type& el) {
+	return hasElementHelper(cont.begin(), cont.end(), el);
+}
+#endif
+
