1fff111a4f3f1b0c841931973861b48c4d49fae7
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-06-07 13:04:21 -0500

    Solve day 7 task 2!

--- src/day_7.cpp
+++ src/day_7.cpp
@@ -15,11 +15,45 @@ class node {
         node(const std::string& name, int weight, const std::vector<std::string> child_names) {
             this->name = name;
             this->weight = weight;
+            this->tower_weight = 0;
             this->child_names = child_names;
         }
+        friend std::ostream& operator<<(std::ostream& out, const node& rhs);
         std::string name;
         int weight;
+        int tower_weight;
         std::vector<std::string> child_names;
+};
+
+std::ostream& operator<<(std::ostream& out, const node& rhs) {
+    out << rhs.name << " (" << rhs.weight << ")[" << rhs.tower_weight << "]";
+    if(rhs.child_names.size() != 0) {
+        out << " -> ";
+        bool first = true;
+        for(auto& name: rhs.child_names) {
+            if(first) {
+                first = false;
+            } else {
+                out << ", ";
+            }
+            out << name;
+        }
+    }
+    return out;
+}
+
+std::vector<node*> node_list;
+std::map<std::string,node*> node_map;
+
+int fill_tower_weight(const std::string& name) {
+    int total = 0;
+    for(auto& child_name: node_map[name]->child_names) {
+        fill_tower_weight(child_name);
+        total += node_map[child_name]->tower_weight;
+    }
+    total += node_map[name]->weight;
+    node_map[name]->tower_weight = total;
+    return total;
 };
 
 int main(int argc, char** argv) {
@@ -41,9 +75,6 @@ int main(int argc, char** argv) {
 
 	// Open input as stream
 	std::ifstream infile(input_filepath);
-
-    std::vector<node*> node_list;
-    std::map<std::string,node*> node_map;
 
     std::regex node_regex("^([a-z]*) \\(([0-9]*)\\)(?: -> (.*))?$");
     std::regex name_regex("([a-z]+)");
@@ -100,6 +131,55 @@ int main(int argc, char** argv) {
     }
 
     std::cout << "Task 1: the bottom program is: " << root_node << std::endl;
+
+    fill_tower_weight(root_node);
+
+    if(verbose) {
+        std::cout << "Node list: " << std::endl;
+        for(auto node: node_list) {
+            std::cout << *node << std::endl;
+        }
+    }
+
+    int new_weight = 0;
+    int target_weight = 0;
+    std::string current_node = root_node;
+    std::string prev_node;
+    while(true) {
+        // Check that each child tower has the same weight
+        std::map<int,int> child_tower_weights;
+        for(auto& child_name: node_map[current_node]->child_names) {
+            int child_tower_weight = node_map[child_name]->tower_weight;
+            child_tower_weights[child_tower_weight] += 1;
+        }
+        if(child_tower_weights.size() != 1) {
+            // Unbalanced -> Save target tower weight, and decend to node which disagrees.
+            int num_children = 0;
+            for(auto& w_p: child_tower_weights) {
+                if(w_p.second > num_children) {
+                    target_weight = w_p.first;
+                    num_children = w_p.second;
+                }
+            }
+            for(auto& child_name: node_map[current_node]->child_names) {
+                if(node_map[child_name]->tower_weight != target_weight) {
+                    current_node = child_name;
+                    break;
+                }
+            }
+        } else {
+            if(target_weight == 0) {
+                std::cerr << "This shouldn't happen!!" << std::endl;
+                return -1;
+            }
+            // This program is balanced, but the previous wasn't!
+            auto it = child_tower_weights.cbegin();
+            new_weight = target_weight - it->first*it->second;
+            break;
+        }
+    }
+
+    std::cout << "Task 2: The new weight of the node should be: " << new_weight << std::endl;
 
 	return 0;
 }
