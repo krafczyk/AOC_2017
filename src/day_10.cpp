a4eba543ace63e7590659d669953c7ff66b37efe
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-06-10 15:29:53 -0500

    Solve Day 10 task 1.

--- src/day_10.cpp
+++ src/day_10.cpp
@@ -11,13 +11,13 @@
 #include "ArgParseStandalone.h"
 #include "utilities.h"
 
-
+typedef size_t type;
 
 int main(int argc, char** argv) {
 	// Parse Arguments
 	std::string input_filepath;
 	bool verbose = false;
-    int size = 256;
+    size_t size = 256;
 	ArgParse::ArgParser Parser("Day 10");
 	Parser.AddArgument("-i/--input", "File defining the input", &input_filepath);
 	Parser.AddArgument("-v/--verbose", "Print Verbose output", &verbose);
@@ -35,8 +35,13 @@ int main(int argc, char** argv) {
 	// Open input as stream
 	std::ifstream infile(input_filepath);
 
-    std::vector<int> list;
-    std::vector<int> lengths;
+    // Create and initialize the list.
+    std::vector<type> list;
+    for(size_t i = 0; i < size; ++i) {
+        list.push_back(i);
+    }
+
+    std::vector<type> lengths;
 
     std::string line;
     if(!std::getline(infile, line)) {
@@ -57,6 +62,42 @@ int main(int argc, char** argv) {
             std::cout << v << std::endl;
         }
     }
+
+    if(verbose) {
+        std::cout << "initial number list" << std::endl;
+        for(auto& v: list) {
+            std::cout << v << std::endl;
+        }
+    }
+
+    // Perform 
+    type skip = 0;
+    type it = 0;
+    type current_pos = 0;
+    while(it < lengths.size()) {
+        size_t len = lengths[it];
+        // Swap elements until we get into the middle.
+        for(size_t I = 0; I < len/2; ++I) {
+            size_t i = (current_pos+I)%size;
+            size_t j = (current_pos+len-I-1)%size;
+            type temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+        if(verbose) {
+            std::cout << "list update" << std::endl;
+            for(auto& v: list) {
+                std::cout << v << std::endl;
+            }
+        }
+
+        // Move current_position
+        current_pos = (current_pos+len+skip)%size;
+        skip += 1;
+        it += 1;
+    }
+
+    std::cout << "Task 1: product: " << list[0]*list[1] << std::endl;
 
 	return 0;
 }
