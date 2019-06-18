0852c8476de2ea358c9632b0262861e1fe8d6861
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-06-18 11:43:07 -0500

    Attempt to solve task 2 day 18.

--- src/day_18.cpp
+++ src/day_18.cpp
@@ -9,21 +9,21 @@
 #include <limits>
 #include <algorithm>
 #include <regex>
+#include <queue>
 #include "ArgParseStandalone.h"
 #include "utilities.h"
 
 typedef long type_t;
 typedef std::unordered_map<std::string,type_t> registers;
+typedef std::vector<type_t> messages;
 
 std::vector<type_t> sounds_played;
-bool rcv_called = false;
-bool first_rcv = true;
-type_t ip = 0;
+const std::string ip = "ip";
 
 class instruction {
     public:
         virtual ~instruction() {}
-        virtual void operator()(registers& r) = 0;
+        virtual bool operator()(registers& r, messages& snd, messages& rcv) = 0;
 };
 
 class inst_2v: public instruction {
@@ -33,7 +33,7 @@ class inst_2v: public instruction {
             this->v = v;
         }
         virtual ~inst_2v() {}
-        virtual void operator()(registers& r) = 0;
+        virtual bool operator()(registers& r, messages& snd, messages& rcv) = 0;
     protected:
         std::string reg;
         type_t v;
@@ -46,7 +46,7 @@ class inst_2r: public instruction {
             this->regr = regr;
         }
         virtual ~inst_2r() {}
-        virtual void operator()(registers& r) = 0;
+        virtual bool operator()(registers& r, messages& snd, messages& rcv) = 0;
     protected:
         std::string regl;
         std::string regr;
@@ -58,7 +58,7 @@ class inst_1v: public instruction {
             this->v = v;
         }
         virtual ~inst_1v() {}
-        virtual void operator()(registers& r) = 0;
+        virtual bool operator()(registers& r, messages& snd, messages& rcv) = 0;
     protected:
         type_t v;
 };
@@ -69,7 +69,7 @@ class inst_1r: public instruction {
             this->reg = reg;
         }
         virtual ~inst_1r() {}
-        virtual void operator()(registers& r) = 0;
+        virtual bool operator()(registers& r, messages& snd, messages& rcv) = 0;
     protected:
         std::string reg;
 };
@@ -77,37 +77,43 @@ class inst_1r: public instruction {
 class sndv: public inst_1v {
     public:
         sndv(type_t v): inst_1v(v) {}
-        void operator()(registers& r [[maybe_unused]]) {
-            sounds_played.push_back(v);
+        bool operator()(registers& r [[maybe_unused]], messages& snd, messages& rcv [[maybe_unused]]) {
+            snd.push_back(v);
+            return true;
         }
 };
 
 class sndr: public inst_1r {
     public:
         sndr(const std::string reg): inst_1r(reg) {}
-        void operator()(registers& r [[maybe_unused]]) {
-            sounds_played.push_back(r[reg]);
+        bool operator()(registers& r, messages& snd, messages& rcv [[maybe_unused]]) {
+            snd.push_back(r[reg]);
+            return true;
         }
 };
 
-class rcvv: public inst_1v {
-    public:
-        rcvv(type_t v): inst_1v(v) {}
-        void operator()(registers& r [[maybe_unused]]) {
-            if(v != 0) {
-                rcv_called = true;
-                sounds_played.push_back(sounds_played.back());
-            }
-        }
-};
+//class rcvv: public inst_1v {
+//    public:
+//        rcvv(type_t v): inst_1v(v) {}
+//        void operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv) {
+//            if(v != 0) {
+//                rcv_called = true;
+//                sounds_played.push_back(sounds_played.back());
+//            }
+//            return true;
+//        }
+//};
 
 class rcvr: public inst_1r {
     public:
         rcvr(const std::string reg): inst_1r(reg) {}
-        void operator()(registers& r [[maybe_unused]]) {
-            if(r[reg] != 0) {
-                rcv_called = true;
-                sounds_played.push_back(sounds_played.back());
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv) {
+            if(rcv.size() != 0) {
+                r[reg] = rcv.front();
+                rcv.erase(rcv.begin());
+                return true;
+            } else {
+                return false;
             }
         }
 };
@@ -115,84 +121,94 @@ class rcvr: public inst_1r {
 class setv: public inst_2v {
     public:
         setv(const std::string& reg, type_t v) : inst_2v(reg, v) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[reg] = v;
+            return true;
         }
 };
 
 class setr: public inst_2r {
     public:
         setr(const std::string& regl, const std::string& regr) : inst_2r(regl, regr) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[regl] = r[regr];
+            return true;
         }
 };
 
 class addv: public inst_2v {
     public:
         addv(const std::string& reg, type_t v): inst_2v(reg, v) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[reg] += v;
+            return true;
         }
 };
 
 class addr: public inst_2r {
     public:
         addr(const std::string& regl, const std::string& regr): inst_2r(regl, regr) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[regl] += r[regr];
+            return true;
         }
 };
 
 class mulv: public inst_2v {
     public:
         mulv(const std::string& reg, type_t v): inst_2v(reg, v) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[reg] *= v;
+            return true;
         }
 };
 
 class mulr: public inst_2r {
     public:
         mulr(const std::string& regl, const std::string& regr): inst_2r(regl, regr) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[regl] *= r[regr];
+            return true;
         }
 };
 
 class modv: public inst_2v {
     public:
         modv(const std::string& reg, type_t v): inst_2v(reg, v) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[reg] %= v;
+            return true;
         }
 };
 
 class modr: public inst_2r {
     public:
         modr(const std::string& regl, const std::string& regr): inst_2r(regl, regr) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             r[regl] %= r[regr];
+            return true;
         }
 };
 
 class jgzv: public inst_2v {
     public:
         jgzv(const std::string& reg, type_t v): inst_2v(reg, v) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             if(r[reg] > 0) {
-                ip += (v-1);
+                r[ip] += (v-1);
             }
+            return true;
         }
 };
 
 class jgzr: public inst_2r {
     public:
         jgzr(const std::string& regl, const std::string& regr): inst_2r(regl, regr) {}
-        void operator()(registers& r) {
+        bool operator()(registers& r, messages& snd [[maybe_unused]], messages& rcv [[maybe_unused]]) {
             if(r[regl] > 0) {
-                ip += (r[regr]-1);
+                r[ip] += (r[regr]-1);
             }
+            return true;
         }
 };
 
@@ -217,7 +233,8 @@ instruction* InstructionFactory(const std::vector<std::string>& words) {
         if(is_register(words[1])) {
             return (instruction*) new rcvr(words[1]);
         } else {
-            return (instruction*) new rcvv(fetch_value<type_t>(words[1]));
+            std::cerr << "No rcvv instruction!!" << std::endl;
+            throw;
         }
     } else if (words[0] == "set") {
         if(is_register(words[2])) {
@@ -278,7 +295,6 @@ int main(int argc, char** argv) {
 	std::ifstream infile(input_filepath);
 
     std::vector<instruction*> program;
-    registers rs;
     std::regex inst_match("([a-z]+) ([a-z\\-0-9]+)(?: ([a-z\\-0-9]+))?");
     std::string line;
     while(std::getline(infile, line)) {
@@ -300,20 +316,72 @@ int main(int argc, char** argv) {
         program.push_back(InstructionFactory(instruction_pieces));
     }
 
-    size_t num = 0;
-    while((ip >= 0) && (ip < (type_t)program.size())) {
+    // First simulation
+    messages single;
+    // Initialize the registers
+    registers single_rs;
+    single_rs["p"] = 0;
+    single_rs[ip] = 0;
+
+    while((single_rs[ip] >= 0) && (single_rs[ip] < (type_t)program.size())) {
         // Execute instruction
-        (*program[ip])(rs);
-        // Advance instruction pointer
-        ip += 1;
-        // Count instruction
-        num += 1;
-        if(rcv_called&&first_rcv) {
-            std::cout << "Task 1: The first recovered frequency is: " << sounds_played.back() << std::endl;
-            first_rcv = false;
-            break;
+        size_t before_size = single.size();
+        if((*program[single_rs[ip]])(single_rs, single, single)) {
+            // Advance to next instruction
+            single_rs[ip] += 1;
+            if(before_size > single.size()) {
+                // A value was received
+                std::cout << "Task 1: The first recovered frequency is: " << single.back() << std::endl;
+                break;
+            }
+        } else {
+            std::cerr << "This shouldn't happen here!!" << std::endl;
+            throw;
         }
     }
+
+    // Second simulation
+    std::map<int,messages> message = { {0, messages()}, {1, messages()}};
+    std::map<int,registers> regs = { {0, registers()}, {1, registers()}};
+    std::map<int,bool> waiting_for_values = {{0, false}, {1, false}};
+    regs[0]["p"] = 0;
+    regs[0][ip] = 0;
+    regs[1]["p"] = 1;
+    regs[1][ip] = 0;
+
+    int current_program = 0;
+    int idle_program = (current_program+1)%2;
+    size_t num_vals_sent = 0;
+    while(true) {
+        // Execute instruction
+        size_t before_size = message[idle_program].size();
+        if((*program[regs[current_program][ip]])(regs[current_program], message[idle_program], message[current_program])) {
+            // Advance to next instruction
+            regs[current_program][ip] += 1;
+            if(message[idle_program].size() > before_size) {
+                if(current_program == 1) {
+                    num_vals_sent += 1;
+                }
+            }
+            // Reset the value waiting
+            if(waiting_for_values[current_program]) {
+                waiting_for_values[current_program] = false;
+            }
+        } else {
+            // We don't have a value to receive.
+            if(waiting_for_values[current_program]) {
+                // We've reached a deadlock
+                break;
+            } else {
+                // Indicate we're not waiting, change programs.
+                waiting_for_values[current_program] = true;
+                current_program = (current_program+1)%2;
+                idle_program = (idle_program+1)%2;
+            }
+        }
+    }
+
+    std::cout << "Task 2: Program 1 sent " << num_vals_sent << " values" << std::endl;
 
 	return 0;
 }
